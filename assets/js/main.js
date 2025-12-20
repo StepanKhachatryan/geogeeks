c14c03042e8ad562883e1074111a6c8b97d0aa32
Stepan Khachatryan <stepan.v.khachatryan@gmail.com>
2025-12-21 02:15:33 +0400

    Convert loadComponent to async function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,9 @@
-function loadComponent(id, file) {
-  fetch(file)
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById(id).innerHTML = data;
-    });
+async function loadComponent(id, file) {
+    const response = await fetch(file);
+    const data = await response.text();
+    document.getElementById(id).innerHTML = data;
 }
 
+// Call them
 loadComponent("header", "components/header.html");
 loadComponent("footer", "components/footer.html");
